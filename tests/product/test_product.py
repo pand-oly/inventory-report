011de84afc7cc52c8d9ff32c2a59ecd6c5b6c32f
pand-oly <hoa98hoa@gmail.com>
2022-12-15 16:11:50 -0300

    feat(Requirement 1): test_cria_produto tests/product/test_product.py

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,16 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1, 'qualquer produto', 'tests S.A',
+        '20-02-2002', '20-06-2002', 666, 'seco',
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == 'qualquer produto'
+    assert product.nome_da_empresa == 'tests S.A'
+    assert product.data_de_fabricacao == '20-02-2002'
+    assert product.data_de_validade == '20-06-2002'
+    assert product.numero_de_serie == 666
+    assert product.instrucoes_de_armazenamento == 'seco'
