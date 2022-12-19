fdf0a5899c77515ba2ba12499eee942a0e77f9cb
pand-oly <hoa98hoa@gmail.com>
2022-12-19 11:25:08 -0300

    feat(Requirement 2): Implement logic to static method "generate" to generate a simple report in the class SimpleReport

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,32 @@
+from datetime import date
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(list: list):
+        today = date.today().strftime("%d-%m-%y")
+
+        oldest_manufacturing = min([
+            item["data_de_fabricacao"]
+            for item in list
+        ])
+
+        nearest_expiration = min([
+            item["data_de_validade"]
+            for item in list
+            if item["data_de_validade"] > today
+        ])
+
+        companies = [
+            item["nome_da_empresa"]
+            for item in list
+        ]
+
+        company_with_more_products = Counter(companies).most_common(1)[0][0]
+
+        return (
+            f"Data de fabricação mais antiga: {oldest_manufacturing}\n"
+            f"Data de validade mais próxima: {nearest_expiration}\n"
+            f"Empresa com mais produtos: {company_with_more_products}"
+        )
