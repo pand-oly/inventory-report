54bcbd5f31f1a90f1d25fc8d39c31f355a9d98ac
pand-oly <hoa98hoa@gmail.com>
2022-12-20 11:38:16 -0300

    feat(Requirement 8): Test for the product class

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,13 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
+    product = Product(
+        1, 'qualquer produto', 'tests S.A',
+        '20-02-2002', '20-06-2002', 666, 'seco',
+    )
+
+    assert product == \
+           "O produto qualquer produto fabricado em 20-02-2002 por tests S.A\
+            com validade até 20-06-2002 precisa ser armazenado seco."
     pass  # Seu teste deve ser escrito aqui
