deee0cb870ff64082cddc663313fceca2c7539bb
pand-oly <hoa98hoa@gmail.com>
2022-12-20 11:38:16 -0300

    feat(Requirement 8): Test for the product class

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,11 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1, 'farinha', 'Farinini',
+        '01-05-2021', '02-06-2023', 666, 'ao abrigo de luz',
+    )
+
+    assert repr(product) == "O produto farinha fabricado em 01-05-2021 por Farinini com validade \
+até 02-06-2023 precisa ser armazenado ao abrigo de luz."
